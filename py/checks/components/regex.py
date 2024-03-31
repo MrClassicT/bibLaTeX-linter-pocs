175f85e06576197b693181e2327b8232d947ac1f
MrClassicT <tristan.cuvelier@student.hogent.be>
2024-03-31 19:18:56 +0200

    regex improvements.

--- py/checks/components/regex.py
+++ py/checks/components/regex.py
@@ -1,2 +1,2 @@
-entry_pattern = re.compile(r'@(\w+)\{([^,]+),\s*(.*?)\}\n\n', re.DOTALL)
+entry_pattern = re.compile(r'@(\w+)\{([^,]+),\s*(.*?)\}\n', re.DOTALL)
 field_pattern = re.compile(r'(\w+)\s*=\s*(?:\{(.*?)\}|(\S+))', re.DOTALL)
